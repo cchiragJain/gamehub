dff330fb259b0b943c76ec51a458fef28d7a86a9
Chirag Jain <cjain8060@gmail.com>
2024-01-18 14:59:39 +0530

    Base Setup of shadcn button Added auth component using clerk Defined css variables for the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-
+import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
-    <p className="text-red-500">Hello There</p>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl='/'/>
+     </div>
   )
 }
